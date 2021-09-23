b5dc8dcb5b75d0d8a80a029d922bf1b26e9e878e
MauriDiPietro <dipietro.jm@gmail.com>
2021-09-23 16:21:04 -0300

    validators

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -5,9 +5,9 @@ export async function createUser(req, res){
     try{
         const {body} = req
         const response = await UsuariosService.createUser(body)  //createUser-->servicio que espera (data)
-        res.status(200).json({response})
+        res.status(200).json(response)
         console.log(response)
     }catch(error){
         res.status(400).send(error.message)
-    }
+    } 
 }
