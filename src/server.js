05fa1bef586646a89aafd4bc70c9ab143ed112cf
MauriDiPietro <dipietro.jm@gmail.com>
2021-09-23 16:23:25 -0300

    validators

--- src/server.js
+++ src/server.js
@@ -12,9 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(morgan('dev'))
 
-app.get('/', (req, res)=>{
-
-})
+app.get('/', (req, res)=>{})
 
 app.use('/usuarios', UsuariosRouter)
 
